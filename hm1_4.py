9ea91e6a5cbaccea9719b481a855bedbbd1d135c
Talant234 <aygtigr123@gmail.com>
2025-10-25 17:49:19 +0000

    sdsfef6

---
+++ hm1_4.py
@@ -0,0 +1,8 @@
+a = [1, 5, 'Good', 'Bad']
+b = [9, 'Blue', 'Red', 11]
+
+print(str(a[0]) + b[2]) 
+print(str(a[1]) + b[1])
+print(a[1] + b[3])
+print(a[1] ** b[3])
+print(a+b)
---
+++ hm1_6.py
@@ -0,0 +1,13 @@
+spisok = []
+
+age = input('Введите свой возраст:')
+gender = input('Введите свой пол:')
+nick = input('Введите свое имя:')
+klass = input('Введите в каком классе вы учитесь:')
+
+spisok.append(age)
+spisok.append(gender)
+spisok.append(nick)
+spisok.append(klass)
+
+print(spisok)
